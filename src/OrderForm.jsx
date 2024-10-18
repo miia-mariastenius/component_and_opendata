4cdf3b91c88994d8488cca3babd442b32ca8d473
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-18 09:35:59 +0300

    Added products and prices in different useStates

--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import OrderInfo from './OrderInfo'
 
 function OrderForm() {
+
+  const [products, setProducts] = useState(['BenQ PD3220U', 'Huawei MateView', 'BenQ PD3420Q', 'Dell U2719DC'])
+  const [productPrice, setProductPrice] = (['1 289€', '699€', '899€', '535€'])
   
   return(
     <div>
