a3e4ca02bf39f10a7632e48270ca4a6c698926c0
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-18 09:09:24 +0300

    Added image and header text to the ComponentUI

--- src/ComponentUI.jsx
+++ src/ComponentUI.jsx
@@ -1,10 +1,12 @@
 import OrderForm from './OrderForm'
+import HeaderImg from './assets/icon-5577198_1280.png'
 
 function ComponentUI() {
 
   return(
     <div>
-      
+      <img src={HeaderImg} alt="Header"  />
+      <h1>Welcome to My Application</h1>
       <OrderForm/>
     </div>
   )
