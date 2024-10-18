569442c68638bba53e4bd4245281db4c5f5165ae
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-18 14:20:31 +0300

    Mapped the products to the dropdown and added useState for the selected product

--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -4,15 +4,20 @@ import OrderInfo from './OrderInfo'
 function OrderForm() {
 
   const [products, setProducts] = useState(['BenQ PD3220U', 'Huawei MateView', 'BenQ PD3420Q', 'Dell U2719DC'])
+  const [selectedProduct, setSelectedProduct] = useState('')
   const [productPrice, setProductPrice] = (['1 289€', '699€', '899€', '535€'])
-  
-  return(
+
+  return (
     <div>
       <h2>Select product</h2>
       <p>Product:</p>
-      <select></select>
+      <select value={selectedProduct} onChange={e => setSelectedProduct(e.target.value)}>
+        {
+          products.map(p => <option key={p} value={p}>{p}</option>)
+        }
+      </select>
       <p>Quantity:</p><button>+</button><button>-</button>
-      <OrderInfo/>
+      <OrderInfo />
     </div>
   )
 }
