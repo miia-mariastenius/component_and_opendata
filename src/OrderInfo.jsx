2a4ed4b6e977d944f4c5342741cfa54b42f56e21
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-22 19:17:06 +0300

    OrderInfo shows props, added basic table css

--- src/OrderInfo.jsx
+++ src/OrderInfo.jsx
@@ -1,8 +1,29 @@
 function OrderInfo({ productName, productPrice, quantity }) {
-  
-  return(
+
+  const calculateTotalPrice = () => {
+    const priceNumber = parseFloat(productPrice.replace(/[^\d.-]/g, ''));
+    return priceNumber * quantity;
+  };
+
+  return (
     <div>
       <h2>Order info</h2>
+      <table className='order-table'>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{productName}</td>
+            <td>{quantity}</td>
+            <td>{calculateTotalPrice()}€</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
