ab2130320e075eb8bd07d91d7f6bbe1241995c82
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-24 14:56:21 +0300

    Added useEffect to fetch the information

--- src/openData.jsx
+++ src/openData.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 function OpenData() {
-  
+
+  const [meal, setMeal] = useState('')
+
+  useEffect(()=> {
+    axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then (resp => {
+      const MealName = resp.data.meals[0].strMeal
+      setMeal(MealName)
+    })
+  },[])
+
   return(
     <div>
-
+      <h3>{meal}</h3>
     </div>
   )
 }
