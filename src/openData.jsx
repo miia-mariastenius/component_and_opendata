c200c1472d71d76c555665f60369e12c1cf20e5c
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-24 19:00:22 +0300

    Added needed useStates and dropdown for selected meals

--- src/openData.jsx
+++ src/openData.jsx
@@ -12,6 +12,9 @@ function OpenData() {
 
   // Searched meal
   const [categories, setCategories] = useState([])
+  const [meals, setMeals] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [selectedMeal, setSelectedMeal] = useState('')
 
 
   //Random meal fetch
@@ -29,10 +32,22 @@ function OpenData() {
   // Categories for dropdown
   useEffect(() => {
     axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then(resp => {
-      setCategories(resp.data.categories)
-    })
+      .then(resp => {
+        setCategories(resp.data.categories)
+      })
   }, [])
+
+  // Meals for dropdown
+  useEffect(() => {
+    if (selectedCategory) {
+      axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`)
+        .then(resp => {
+          setMeals(resp.data.meals)
+        })
+    }
+  }, [selectedCategory])
+
+  // Update selected meal to site
 
   return (
     <div className="container">
@@ -43,19 +58,36 @@ function OpenData() {
         <img src={mealImg} className="meal-img" />
         <p className="recipe">{recipe}</p>
       </div>
+
       <div className="search">
         <h2>Search for a meal</h2>
         <p>Category:</p>
-        <select onChange={(e) => setCategories(e.target.value)}>
+        <select onChange={(e) => setSelectedCategory(e.target.value)}>
           <option value="">Select a Category</option>
           {
-            categories.map(c => (
-              <option key={c.idCategory} value={c.strCategory}>
-                {c.strCategory}
+            categories.map(category => (
+              <option key={category.idCategory} value={category.strCategory}>
+                {category.strCategory}
               </option>
             ))
           }
         </select>
+
+        {selectedCategory && (
+          <>
+            <p>Meal:</p>
+            <select onChange={(e) => setSelectedMeal(e.target.value)}>
+              <option value="">Select a Meal</option>
+              {
+                meals.map(meal => (
+                  <option key={meal.idMeal} value={meal.idMeal}>
+                    {meal.strMeal}
+                  </option>
+                ))
+              }
+            </select>
+          </>
+        )}
       </div>
     </div>
   )
