9dcfadd48852eb5bc2872b2c0048ec468cc93438
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-24 15:56:21 +0300

    Changed the layout of the page and added a search dropdown

--- src/openData.jsx
+++ src/openData.jsx
@@ -9,24 +9,31 @@ function OpenData() {
   const [recipe, setRecipe] = useState('')
   const [mealImg, setMealImg] = useState(null)
 
-  useEffect(()=> {
+  useEffect(() => {
     axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then (resp => {
-      const mealData = resp.data.meals[0]
-      setMeal(mealData.strMeal)
-      setMealType(mealData.strCategory)
-      setRecipe(mealData.strInstructions)
-      setMealImg(mealData.strMealThumb)
-    })
-  },[])
+      .then(resp => {
+        const mealData = resp.data.meals[0]
+        setMeal(mealData.strMeal)
+        setMealType(mealData.strCategory)
+        setRecipe(mealData.strInstructions)
+        setMealImg(mealData.strMealThumb)
+      })
+  }, [])
 
-  return(
-    <div>
-      <h1>Meal Generator</h1>
-      <h3 className="title">{meal}</h3>
-      <p className="title">({mealType})</p><br/>
-      <img src={mealImg} className="meal-img"/>
-      <p className="recipe">{recipe}</p>
+  return (
+    <div className="container">
+      <div className="random-meal">
+        <h1>Meal Generator</h1>
+        <h3 className="title">{meal}</h3>
+        <p className="title">({mealType})</p><br />
+        <img src={mealImg} className="meal-img" />
+        <p className="recipe">{recipe}</p>
+      </div>
+      <div className="search">
+        <h2>Search for a meal</h2>
+        <p>Category:</p>
+        <select></select>
+      </div>
     </div>
   )
 }
