da7f7d1830b38f58473782144942af4d784b655f
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-24 14:31:15 +0300

    Created openData file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import OpenData from './openData.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <OpenData />
   </StrictMode>,
 )
---
+++ src/openData.jsx
@@ -0,0 +1,10 @@
+function OpenData() {
+  
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default OpenData
