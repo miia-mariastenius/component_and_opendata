922a4baf7e30a59b368afe8af772614b44b19662
miia-mariastenius <n3stmi00@studens.oamk.fi>
2024-10-24 15:37:29 +0300

    Added fetched information and added some styling

--- src/openData.jsx
+++ src/openData.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import './openData.css'
 
 function OpenData() {
 
   const [meal, setMeal] = useState('')
+  const [mealType, setMealType] = useState('')
+  const [recipe, setRecipe] = useState('')
+  const [mealImg, setMealImg] = useState(null)
 
   useEffect(()=> {
     axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
     .then (resp => {
-      const MealName = resp.data.meals[0].strMeal
-      setMeal(MealName)
+      const mealData = resp.data.meals[0]
+      setMeal(mealData.strMeal)
+      setMealType(mealData.strCategory)
+      setRecipe(mealData.strInstructions)
+      setMealImg(mealData.strMealThumb)
     })
   },[])
 
   return(
     <div>
-      <h3>{meal}</h3>
+      <h1>Meal Generator</h1>
+      <h3 className="title">{meal}</h3>
+      <p className="title">({mealType})</p><br/>
+      <img src={mealImg} className="meal-img"/>
+      <p className="recipe">{recipe}</p>
     </div>
   )
 }
